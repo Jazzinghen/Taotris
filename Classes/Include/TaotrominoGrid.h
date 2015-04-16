280a238267a35d411966b53ed3f0a6c3601393c4
Michele Bianchi <jazzinghen@gmail.com>
2015-04-16 15:46:31 +0900

    Ok, generated the first few functions for the Grid definitions.
    Now onto integrating grids into Taotrominos

--- Classes/Include/TaotrominoGrid.h
+++ Classes/Include/TaotrominoGrid.h
@@ -5,15 +5,29 @@
  * Created on 16 April 2015, 13:54
  */
 
+#include <array>
+#include "Taotromino.h"
+
 #ifndef TAOTROMINOGRID_H
 #define	TAOTROMINOGRID_H
 
 class TaotrominoGrid {
 public:
-    TaotrominoGrid();
+    TaotrominoGrid(std::array<Taotromino::taotromino_t, (std::size_t) 4> first,
+                   std::array<Taotromino::taotromino_t, (std::size_t) 4> second,
+                   std::array<Taotromino::taotromino_t, (std::size_t) 4> third,
+                   std::array<Taotromino::taotromino_t, (std::size_t) 4> fourth);
+    
     TaotrominoGrid(const TaotrominoGrid& orig);
     virtual ~TaotrominoGrid();
+    
+    std::array<Taotromino::taotromino_t, (std::size_t) 4> getColum(int x) {
+        return gridDefinition[x];
+    };
+    
 private:
+    // gridDefinition are defined by columns, starting from the top (i.e. 0 is the first column to the right)
+    std::array<std::array<Taotromino::taotromino_t, (std::size_t) 4>, (std::size_t) 4> gridDefinition;
 
 };
 
--- Classes/TaotrominoGrid.cpp
+++ Classes/TaotrominoGrid.cpp
@@ -5,9 +5,21 @@
  * Created on 16 April 2015, 13:54
  */
 
-#include "TaotrominoGrid.h"
+#include <array>
 
-TaotrominoGrid::TaotrominoGrid() {
+#include "Include/TaotrominoGrid.h"
+#include "Include/Taotromino.h"
+
+TaotrominoGrid::TaotrominoGrid(std::array<Taotromino::taotromino_t, (std::size_t) 4> first,
+                               std::array<Taotromino::taotromino_t, (std::size_t) 4> second,
+                               std::array<Taotromino::taotromino_t, (std::size_t) 4> third,
+                               std::array<Taotromino::taotromino_t, (std::size_t) 4> fourth) {
+    
+    gridDefinition[0] = first;
+    gridDefinition[1] = second;
+    gridDefinition[2] = third;
+    gridDefinition[3] = fourth;
+
 }
 
 TaotrominoGrid::TaotrominoGrid(const TaotrominoGrid& orig) {
