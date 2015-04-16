f529170d2f15bbb31228f419ff6d4cccc9178b3a
Michele Bianchi <jazzinghen@gmail.com>
2015-04-16 14:04:13 +0900

    Started working on the Grid definition class

---
+++ Classes/Include/TaotrominoGrid.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   TaotrominoGrid.h
+ * Author: jazzinghen
+ *
+ * Created on 16 April 2015, 13:54
+ */
+
+#ifndef TAOTROMINOGRID_H
+#define	TAOTROMINOGRID_H
+
+class TaotrominoGrid {
+public:
+    TaotrominoGrid();
+    TaotrominoGrid(const TaotrominoGrid& orig);
+    virtual ~TaotrominoGrid();
+private:
+
+};
+
+#endif	/* TAOTROMINOGRID_H */
+
---
+++ Classes/TaotrominoGrid.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   TaotrominoGrid.cpp
+ * Author: jazzinghen
+ * 
+ * Created on 16 April 2015, 13:54
+ */
+
+#include "TaotrominoGrid.h"
+
+TaotrominoGrid::TaotrominoGrid() {
+}
+
+TaotrominoGrid::TaotrominoGrid(const TaotrominoGrid& orig) {
+}
+
+TaotrominoGrid::~TaotrominoGrid() {
+}
+
